3198f9711d77c229b15179efa0c881234cb7087f
Moniruzzaman Sujon <git.sujon@gmail.com>
2023-04-01 12:37:09 +0600

    Connection build with openAI done

--- index.js
+++ index.js
@@ -1,27 +1,49 @@
-const dotenv = require('dotenv').config()
+const dotenv = require("dotenv").config();
+
+const { Client, IntentsBitField } = require("discord.js");
+
+const { Configuration, OpenAIApi } = require("openai");
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.on("ready", () => {
+  console.log(`The bot is Running as ${client.user.tag}`);
+});
+
+const configuration = new Configuration({
+  apiKey: process.env.API_KEY,
+});
+
+const openai = new OpenAIApi (configuration)
 
 
-const {Client, IntentsBitField} = require('discord.js')
-const client= new Client({
-    intents: [
-       IntentsBitField.Flags.Guilds,
-       IntentsBitField.Flags.GuildMessages,
-       IntentsBitField.Flags.MessageContent,
-       
-    ]
-})
+client.on("messageCreate", async(message) => {
+ if(message.author.bot) return;
+//  if(message.channel.id !== process.env.SERVER_ID) return;
+ if(message.content.startsWith('!')) return;
+ 
+ let conversationLog = [{role: 'system', content: 'You are a computer Engineer'}]
 
-client.on('ready', ()=> {
-    console.log(`The bot is Running as ${client.user.tag}`)
-})
+ conversationLog.push ({
+    role: 'user',
+    content: message.content,
+ })
 
-client.on('messageCreate', (message)=> {
-    if(message.content === 'hey'){
-        message.reply("Hello")
-    }
-})
+ await message.channel.sendTyping();
+ 
+ const result = await openai.createChatCompletion({
+    model:'gpt-3.5-turbo',
+    messages:conversationLog,
+ })
 
+ message.reply(result.data.choices[0].message)
 
+});
 
-
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN);
